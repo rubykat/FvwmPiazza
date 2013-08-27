7cd3b9eddab42275340cf4ee6b9add384878f936
Kathryn Andersen <perlkat@katspace.org>
2013-08-27 16:30:08 +1000

    Improve the calculate_ratios method.
    	modified:   Layouts.pm
    
    Pull in improvements from "layout" branch for calculating ratios.
    Now ratios can be actual ratios, not just percentages.

--- lib/FvwmPiazza/Layouts.pm
+++ lib/FvwmPiazza/Layouts.pm
@@ -79,25 +79,71 @@ sub calculate_ratios {
 	and $args{ratios} =~ /^([\d:]+)/)
     {
 	my $ratio_str = $1;
-	# The ratio arguments are in percent.
 	my @r_args = split(':', $ratio_str);
-	my $percent_left = 100;
-	my $cols_left = $num_sets;
-	for (my $i=0; $i < $num_sets; $i++)
+
+	my $ratio_total = 0;
+	for (my $i=0; $i < @r_args; $i++)
 	{
-	    if ($i < @r_args)
+	    $ratio_total += $r_args[$i];
+	}
+
+	# If the number of ratio args is greater than or equal the
+	# number of sets, then treat it as a straight ratio,
+	# taking $num_sets arguments for the ratio
+	if ($num_sets <= @r_args)
+	{
+	    if ($num_sets < @r_args)
 	    {
-		$ratios[$i] = $r_args[$i] / 100;
-		$percent_left -= $r_args[$i];
-		$cols_left--;
+		$ratio_total = 0;
+		for (my $i=0; $i < $num_sets; $i++)
+		{
+		    $ratio_total += $r_args[$i];
+		}
 	    }
-	    else
+	    for (my $i=0; $i < $num_sets; $i++)
 	    {
-		$ratios[$i] = ($percent_left / $cols_left) / 100;
+		$ratios[$i] = $r_args[$i] / $ratio_total;
+	    }
+	}
+	# If there is only one ratio-arg,
+	# treat it as a percent, and divide the rest evenly
+	elsif ($num_sets > @r_args and @r_args == 1)
+	{
+	    my $percent_left = 100;
+	    my $cols_left = $num_sets;
+	    for (my $i=0; $i < $num_sets; $i++)
+	    {
+		if ($i < @r_args)
+		{
+		    $ratios[$i] = $r_args[$i] / 100;
+		    $percent_left -= $r_args[$i];
+		    $cols_left--;
+		}
+		else
+		{
+		    $ratios[$i] = ($percent_left / $cols_left) / 100;
+		}
+	    }
+	}
+	else
+	{
+	    # The number of ratio args is less than the number of sets.
+	    # Divide the first N by the ratio, and the rest evenly.
+	    $ratio_total = $ratio_total * ($num_sets / @r_args);
+	    for (my $i=0; $i < $num_sets; $i++)
+	    {
+		if ($i < @r_args)
+		{
+		    $ratios[$i] = $r_args[$i] / $ratio_total;
+		}
+		else
+		{
+		    $ratios[$i] = 1/$num_sets;
+		}
 	    }
 	}
     }
-    else
+    else # divide everything evenly
     {
 	for (my $i=0; $i < $num_sets; $i++)
 	{
